f6c83f43bf4136f269c31ab8de6c1f47fb3b8632
miguel <mapavone@yahoo.com>
2025-06-19 15:09:32 +0200

    Added new tests with parameters overrides

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 # Welcome to AIChat Tests
 
 Here you will find integration tests for AIChat Backend
+
+Steps to build the project:
+1) export CODEARTIFACT_AUTH_TOKEN=$(aws codeartifact get-authorization-token \
+   --domain personal \
+   --domain-owner 472156860409 \
+   --query authorizationToken \
+   --output text)
+2) ./gradlew build
--- src/test/java/com/aichatrestapi/clients/AIChatPredictTest.java
+++ src/test/java/com/aichatrestapi/clients/AIChatPredictTest.java
@@ -9,17 +9,24 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AIChatPredictTest {
     private AIChatClient client;
 
+    private static final String HEADER_TOKEN = "X-HUGGING-FACE-TOKEN";
+    private static final String HEADER_MODEL = "X-MODEL";
+    private static final String HEADER_BASE_URL = "X-BASE-URL";
+    private static final String HEADER_DO_TYPE = "X-DO-TYPE";
+
     @BeforeClass
     public void setup() {
         ParameterStoreClient parameterStoreClient = new ParameterStoreClient();
-        String token =
+        String apiUrl =
                 parameterStoreClient.getParameterValue("/aichat/backend/apiUrl");
-        this.client = new AIChatClient(token);
+        this.client = new AIChatClient(apiUrl);
     }
 
     @Test(dataProviderClass = DataProviderClass.class, dataProvider = "messagesProvider")
@@ -29,4 +36,31 @@ public class AIChatPredictTest {
         Assert.assertFalse(responseBody.getMessage().contains("<think>"));
         Assert.assertFalse(responseBody.getMessage().contains("</think>"));
     }
+
+    @Test(dataProviderClass = DataProviderClass.class, dataProvider = "messagesProvider")
+    public void predict_withHuggingFace_nebius_meta_llama_happy_path(List<PredictRequestMessage> messages) {
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put(HEADER_BASE_URL, "https://router.huggingface.co/nebius/v1/chat/completions");
+        headers.put(HEADER_MODEL, "meta-llama/Meta-Llama-3.1-8B-Instruct-fast");
+        headers.put(HEADER_DO_TYPE, "HuggingFaceChatResponse");
+        PredictResponseBody responseBody = client.callPostPredict(new PredictRequestBody(messages), headers);
+        Assert.assertFalse(responseBody.getMessage().isEmpty());
+        Assert.assertFalse(responseBody.getMessage().contains("<think>"));
+        Assert.assertFalse(responseBody.getMessage().contains("</think>"));
+    }
+    @Test(dataProviderClass = DataProviderClass.class, dataProvider = "messagesProvider")
+
+    public void predict_withHuggingFace_together_deepseek_r1_happy_path(List<PredictRequestMessage> messages) {
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put(HEADER_BASE_URL, "https://router.huggingface.co/together/v1/chat/completions");
+        headers.put(HEADER_MODEL, "deepseek-ai/DeepSeek-R1");
+        headers.put(HEADER_DO_TYPE, "HuggingFaceChatResponse");
+        PredictResponseBody responseBody = client.callPostPredict(new PredictRequestBody(messages), headers);
+        Assert.assertFalse(responseBody.getMessage().isEmpty());
+        Assert.assertFalse(responseBody.getMessage().contains("<think>"));
+        Assert.assertFalse(responseBody.getMessage().contains("</think>"));
+    }
+
 }
